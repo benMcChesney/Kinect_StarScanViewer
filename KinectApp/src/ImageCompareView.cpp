0497c4a04ebe0c02d89e1c674e02be0dc5fb4234
Ben McChesney <ben.mcchesney@gmail.com>
2015-01-10 21:17:39 -0800

    ImageCompareView mask between detail images working correctly.

--- KinectApp/src/ImageCompareView.cpp
+++ KinectApp/src/ImageCompareView.cpp
@@ -12,7 +12,7 @@
 
 void ImageCompareView::setup ( )
 {
-	mask.setup( "shaders/composite" , ofRectangle( 0 , 0 , 1024, 1024 ) ) ; 
+	mask.setup( "shaders/composite_rgb" , ofRectangle( 0 , 0 , 1024, 1024 ) ) ; 
 	leftView.setup() ; 
 	rightView.setup() ; 
 }
@@ -26,30 +26,41 @@ void ImageCompareView::update( )
 	
 void ImageCompareView::draw( ) 
 {
+	//Draw Left active area
+	leftView.maskFbo.begin() ; 
+		ofSetColor( ofColor::black ) ; 
+		ofRect( 0 , 0 , 1024.0f , ofGetHeight() ) ; 
+		ofSetColor( ofColor::white ) ; 
+		ofRect( 0 , 0 , 1024.0f * normalizedAmount , ofGetHeight() ) ; 
+	leftView.maskFbo.end() ; 
+
+	//Draw Right active area
+	rightView.maskFbo.begin( ) ; 
+		//float _y = ofGetHeight() /2 ; 
+		ofSetColor( ofColor::black ) ; 
+		ofRect( 0 , 0 , 1024.0f , ofGetHeight() ) ; 
+		ofSetColor( ofColor::white ) ; 
+		ofRect( 1024.0f * normalizedAmount  , 0 , 1024.0f * ( 1.0f - normalizedAmount ) , ofGetHeight() ) ; 
+	rightView.maskFbo.end( ) ; 
+	
+	
 	ofPushMatrix() ; 
 		float offsetX = ( ofGetWidth() - 1024 ) / 2 ;
 		ofTranslate( offsetX , 0  ) ; 
+		mask.drawMask( rightView.detailImage->image.getTextureReference() , rightView.maskFbo.getTextureReference() , 0 , 0 , 1.0f ) ; 
+		mask.drawMask( leftView.detailImage->image.getTextureReference() , leftView.maskFbo.getTextureReference() , 0 , 0 , 1.0f ) ; 
+		//rightView.maskFbo.draw( 0 , 0 ) ; 
+		
 
-		//Draw Left active area
-		ofSetColor( ofColor::black ) ; 
-		ofRect( 0 , 0 , 1024.0f , ofGetHeight() / 2 ) ; 
-		ofSetColor( ofColor::white ) ; 
-		ofRect( 0 , 0 , 1024.0f * normalizedAmount , ofGetHeight() / 2 ) ; 
-
-		//Draw Right active area
-		float _y = ofGetHeight() /2 ; 
-		ofSetColor( ofColor::black ) ; 
-		ofRect( 0 , _y , 1024.0f , ofGetHeight() / 2 ) ; 
-		ofSetColor( ofColor::white ) ; 
-		ofRect( 1024.0f * normalizedAmount  , _y , 1024.0f * ( 1.0f - normalizedAmount ) , ofGetHeight() / 2 ) ; 
+		
+		
 
 	ofPopMatrix() ; 
-
-
 }
 	
 void ImageCompareView::drawDebug( ) 
 {
-
+	stringstream ss ; 
+	//ss << "rightView : " << rightView.detailImage->getAl
 }
 
